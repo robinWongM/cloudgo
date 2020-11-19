e81e6bdfb92707aa707318f1a52b49a089404d2d
Robin Wong <chaowang0313@gmail.com>
2020-11-19 14:59:02 +0000

    chore: add useful comments

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
+	// Create a gin router with default middleware:
+	// logger and recovery (crash-free) middleware
 	r := gin.Default()
+
+	// Create a gin route with a parameter called `id`
 	r.GET("/hello/:id", func(c *gin.Context) {
+		// Respond with a JSON, serialized from a map[string]interface{}
+		// gin.H is a shortcut for map[string]interface{}
 		c.JSON(200, gin.H{
-			"Test": "Hello " + c.Param("id"),
+			"Test": "Hello " + c.Param("id"), // Obtain value for parameter `:id`
 		})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// Listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// Or the port you specified in the environment variable `PORT`
+	r.Run()
 }
